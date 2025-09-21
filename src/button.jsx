ec08e18aea5c0cfd7e8518518d3b507dd659c01e
Aryan Jaiswal <kumararayan67@gmail.com>
2025-09-21 18:42:21 +0530

    External CSS

--- src/button.jsx
+++ src/button.jsx
@@ -1,19 +1,8 @@
 
 const Button = () => {
 
-    const styles = {
-        backgroundColor: "hsl(200, 100%, 50%)",
-        color: "white",
-        border: "none",
-        padding: "10px 20px",
-        borderRadius: "5px",
-        cursor: "pointer",
-        fontSize: "16px",
-        marginTop: "20px",
-    }
-
     return(
-        <button style ={styles}>Click me</button>
+        <button className="btn">Click me</button>
     );
 }
 
